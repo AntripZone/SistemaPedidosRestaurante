38fecbdcd56370c9c1d41dd1400e5b7c64b18464
AdrianAlvaT <adrian_alvat@hotmail.com>
2026-08-24 15:45:54 -0500

    xsdfds

--- src/controllers/pedidosControllers.ts
+++ src/controllers/pedidosControllers.ts
@@ -13,7 +13,23 @@ export async function getPedidos(req:Request, res: Response) {
         console.error("Error al consultar PostgreSQL: ");
         res.status(500).json({
             message: "Error al conectar al Base de Datos.",
-        })
+        });
+    }  
+}
+
+export async function getPedidosId(req: Request, res: Response) {
+    try{
+        const id = Number(req.params.id);
+        if(isNaN(id)){
+            res.status(400).json({error: "El id debe ser un valor numerico"});
+        }
+        const resu = await pool.query("SELECT * FROM pedidos WHERE id = $1", [id]);
+        if(resu.rows.length === 0){
+            res.status(404).json({error: "Pedido no encontrado"});
+            return;
+        }
+        res.json(resu.rows[0]);
+    }catch(error: any){
+        res.status(500).json({error: error.message});
     }
-    
 }
