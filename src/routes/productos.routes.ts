88447001644233db1ec59b86a50aaa8c7dd6dbc8
Pamela Sessa <karinasessa@gmail.com>
2026-08-24 18:34:32 -0300

    controller,routes  de producto y config db

--- src/routes/productos.routes.ts
+++ src/routes/productos.routes.ts
@@ -1,163 +1,23 @@
 import { Router } from "express";
-import productos from "../productos.json" with { type: "json" };
+
+import {
+  getProductos,
+  getProductoPorId,
+  crearProducto,
+  actualizarProducto,
+  eliminarProducto
+} from "../controllers/productos.controller.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  // #swagger.tags = ['PRODUCTOS ✅ ']
-  // #swagger.description = 'obtiene la lista de productos'
-  // #swagger.description = permite filtrar los productos por categoria
-  const { categoria } = req.query;
+router.get("/", getProductos);
 
-  if (categoria) {
-    const productosFiltrados = productos.filter(
-      (producto) => producto.categoria === categoria,
-    );
+router.get("/:id", getProductoPorId);
 
-    return res.json(productosFiltrados);
-  }
+router.post("/", crearProducto);
 
-  res.json(productos);
-});
+router.put("/:id", actualizarProducto);
 
-router.get("/:id", (req, res) => {
-  /*  // #swagger.tags = ['PRODUCTOS ID 🔑']
-  // #swagger.description = 'Obtiene la informacion de un producto en especifico por su id'
-    #swagger.parameters['id'] = {
-          in: 'path',
-          description: 'ID del producto a buscar',
-          required: true,
-          type: 'integer'
-  }  */
-  const id = Number(req.params.id);
-
-  const producto = productos.find((producto) => producto.id === id);
-
-  if (!producto) {
-    return res.status(404).json({
-      mensaje: "Producto no encontrado",
-    });
-  }
-
-  res.json(producto);
-});
-
-router.post("/", (req, res) => {
-  /*
-      #swagger.tags = ['CREAR UN PRODUCTO 🏷️']
-      #swagger.summary = 'crear un producto nuevo'
-      #swagger.parameters['body'] = {
-        in: 'body',
-        description: 'Datos para crear un producto nuevo',
-        required: true,
-        schema: {
-          $nombre: "coca cola",
-          $categoria: "bebida",
-          $precio: 15,
-          disponible: "true"
-        }
-      }
-    */
-  const { nombre, categoria, precio, disponible } = req.body;
-
-  if (precio <= 0) {
-    return res.status(400).json({
-      mensaje: "El precio debe ser mayor a 0",
-    });
-  }
-
-  const nuevoProducto = {
-    id: productos.length + 1,
-    nombre: nombre,
-    categoria: categoria,
-    precio: precio,
-    disponible: disponible,
-  };
-
-  productos.push(nuevoProducto);
-
-  res.status(201).json(nuevoProducto);
-});
-router.put("/:id", (req, res) => {
-  /*
-    #swagger.tags = ['ACTUALIZAR UN PRODUCTO 🧾']
-    #swagger.summary = 'actualizar un producto existente'
-    #swagger.parameters['id'] = {
-      in: 'path',
-      description: 'id del producto a actualizar',
-      required: true,
-      type: 'integer'
-    }
-    #swagger.parameters['body'] = {
-      in: 'body',
-      description: 'Datos a actualizar del producto',
-      required: true,
-      schema: {
-        $nombre: "coca cola",
-          $categoria: "bebida",
-          $precio: 15,
-          disponible: "true"
-      }
-    }
-  */
-  const id = Number(req.params.id);
-
-  const producto = productos.find((producto) => producto.id === id);
-
-  if (!producto) {
-    return res.status(404).json({
-      mensaje: "Producto no encontrado",
-    });
-  }
-
-  const { nombre, precio, disponible } = req.body;
-
-  if (precio !== undefined && precio <= 0) {
-    return res.status(400).json({
-      mensaje: "El precio debe ser mayor a 0",
-    });
-  }
-
-  if (nombre !== undefined) {
-    producto.nombre = nombre;
-  }
-
-  if (precio !== undefined) {
-    producto.precio = precio;
-  }
-
-  if (disponible !== undefined) {
-    producto.disponible = disponible;
-  }
-
-  res.json(producto);
-});
-router.delete("/:id", (req, res) => {
-  /*
-    #swagger.tags = ['ELIMINAR UN PRODUCTO ❌']
-    #swagger.summary = 'eliminar un producto'
-    #swagger.parameters['id'] = {
-      in: 'path',
-      description: 'ID del producto a eliminar',
-      required: true,
-      type: 'integer'
-    }
-  */
-  const id = Number(req.params.id);
-
-  const indice = productos.findIndex((producto) => producto.id === id);
-
-  if (indice === -1) {
-    return res.status(404).json({
-      mensaje: "Producto no encontrado",
-    });
-  }
-
-  productos.splice(indice, 1);
-
-  res.json({
-    mensaje: "Producto eliminado correctamente",
-  });
-});
+router.delete("/:id", eliminarProducto);
 
 export default router;
